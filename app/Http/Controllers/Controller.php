52beb5a81261b071507722e08ba4df50a588ded4
Ali Raza <aliraza92>
2022-11-12 23:28:48 +0500

    Project Initilization

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use App\Models\User;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests;
+    use DispatchesJobs;
+    use ValidatesRequests;
+
+    public function getUsers()
+    {
+        return User::all();
+    }
+}
